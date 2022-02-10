f60b6b2141dacd94b0de74a3f00f3a7114cf3697
Shana Shi <shanashi@Shanas-MacBook-Pro.local>
2022-02-09 18:17:08 -0800

    第二次

---
+++ Dish.java
@@ -0,0 +1,15 @@
+package menu;
+
+public class Dish {
+
+    int id;
+    String name;
+    double price;
+
+    public Dish(int id,String name,double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+}
